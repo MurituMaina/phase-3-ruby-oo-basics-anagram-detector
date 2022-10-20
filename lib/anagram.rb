7c9744b75c44a2535a86528485594142abda69b4
MurituMaina <vincent.maina@student.moringaschool.com>
2022-10-20 08:27:55 +0300

    add anagram complete tasks

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+def initialize word
+@word = word
+end
+
+    def match(words)
+    matches = []
+     words.each do  |i|
+       if (@word.chars).sort == (i.chars).sort
+        matches << i
+        p matches
+        end
+     end
+    return matches
+    end
+end
